894c1dbaddfec1477ce30ee71f9b0f16b866aad8
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-09 12:49:00 -0500

    changing functions

--- index.js
+++ index.js
@@ -9,101 +9,143 @@ const { mainModule } = require('process');
 
 let cards = []; 
 
-inquirer.prompt([
-    {
-        type: "input", 
-        message: "Please enter a team manager's name. ",
-        name: "managerName"
-    }, 
-    {
-        type: "input", 
-        message: "Please enter the manager's employee ID. ",
-        name: "managerID"
-    }, 
-    {
-        type: "input", 
-        message: "Please enter the manager's email address. ",
-        name: "managerEmail"
-    },
-    {
-        type: "input", 
-        message: "Please enter the manager's office number.",
-        name: "managerOfficeNumber"
-    },
-    {
-        tyep: "checkbox", 
-        message: "What's your next step?",
-        name: "nextSteps",
-        choices: ["Add an engineer", "Add an intern", "Finish building my team"]
-    }, 
-])
-.then((data) => {
-    if(data.nextSteps === "Add an engineer") {
-        inquirer.prompt([
-            {
-                type: "input", 
-                message: "Please enter an engineer's name. ",
-                name: "engineerName"
-            }, 
-            {
-                type: "input", 
-                message: "Please enter the engineer's employee ID. ",
-                name: "engineerID"
-            }, 
-            {
-                type: "input", 
-                message: "Please enter the engineer's email address. ",
-                name: "engineerEmail"
-            },
-            {
-                type: "input", 
-                message: "Please enter the engineer's GitHub username.",
-                name: "engineerGitHub"
-            },
-            {
-                tyep: "checkbox", 
-                message: "What's your next step?",
-                name: "nextSteps",
-                choices: ["Add an engineer", "Add an intern", "Finish building my team"]
-            }, 
-        ])
-        .then((data) => {
-            if(data.nextSteps === "Add an intern") {
-                inquirer.prompt([
-                    {
-                        type: "input", 
-                        message: "Please enter an intern's name. ",
-                        name: "internName"
-                    }, 
-                    {
-                        type: "input", 
-                        message: "Please enter the intern's employee ID. ",
-                        name: "internID"
-                    }, 
-                    {
-                        type: "input", 
-                        message: "Please enter the intern's email address. ",
-                        name: "internEmail"
-                    },
-                    {
-                        type: "input", 
-                        message: "Please enter the intern's school.",
-                        name: "internSchool"
-                    },
-                    {
-                        tyep: "checkbox", 
-                        message: "What's your next step?",
-                        name: "nextSteps",
-                        choices: ["Add an engineer", "Add an intern", "Finish building my team"]
-                    }, 
-                ])
-                .then((data) => {
-                    if(data.nextSteps === "Finish building my team") {
-                        return "Thank you for entering the information. ";
-                }
-            })
-        }
-    })
+//seperating the functions for readability. 
+//add a manager: 
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input", 
+            message: "Please enter a team manager's name. ",
+            name: "managerName"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the manager's employee ID. ",
+            name: "managerID"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the manager's email address. ",
+            name: "managerEmail"
+        },
+        {
+            type: "input", 
+            message: "Please enter the manager's office number.",
+            name: "managerOfficeNumber"
+        },
+        {
+            tyep: "checkbox", 
+            message: "What's your next step?",
+            name: "nextSteps",
+            choices: ["Add a manager", "Add an engineer", "Add an intern", "Finish building my team"]
+        }, 
+    ])
+    .then((data) => {
+        if(data.nextSteps === "Add a manager") {
+            addManager();
+         };
+        if(data.nextSteps === "Add an engineer") {
+           addEngineer();
+        };
+        if(data.nextSteps === "Add an intern") {
+           addIntern();
+        };
+        if(data.nextSteps === "Finish building my team") {
+            return "Thank you for entering the information. "
+        };
+}); 
+
+
+//add an engineer: 
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input", 
+            message: "Please enter an engineer's name. ",
+            name: "engineerName"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the engineer's employee ID. ",
+            name: "engineerID"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the engineer's email address. ",
+            name: "engineerEmail"
+        },
+        {
+            type: "input", 
+            message: "Please enter the engineer's GitHub username.",
+            name: "engineerGitHub"
+        },
+        {
+            tyep: "checkbox", 
+            message: "What's your next step?",
+            name: "nextSteps",
+            choices: ["Add a manager", "Add an engineer", "Add an intern", "Finish building my team"]
+        }, 
+    ])
+    .then((data) => {
+        if(data.nextSteps === "Add a manager") {
+            addManager();
+        };
+        if(data.nextSteps === "Add an engineer") {
+           addEngineer();
+        };
+        if(data.nextSteps === "Add an intern") {
+           addIntern();
+        };
+        if(data.nextSteps === "Finish building my team") {
+            return "Thank you for entering the information. "
+        };
+}); 
+
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input", 
+            message: "Please enter an intern's name. ",
+            name: "internName"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the intern's employee ID. ",
+            name: "internID"
+        }, 
+        {
+            type: "input", 
+            message: "Please enter the intern's email address. ",
+            name: "internEmail"
+        },
+        {
+            type: "input", 
+            message: "Please enter the intern's school.",
+            name: "internSchool"
+        },
+        {
+            tyep: "checkbox", 
+            message: "What's your next step?",
+            name: "nextSteps",
+            choices: ["Add an engineer", "Add an intern", "Finish building my team"]
+        }, 
+    ])
+    .then((data) => {
+        if(data.nextSteps === "Add a manager") {
+            addManager();
+        };
+        if(data.nextSteps === "Add an engineer") {
+           addEngineer();
+        };
+        if(data.nextSteps === "Add an intern") {
+           addIntern();
+        };
+        if(data.nextSteps === "Finish building my team") {
+            return "Thank you for entering the information. "
+        };
+}); 
+
 
     const manager = new Manager(`${data.managerName}`, `${data.managerID}`, `${data.managerEmail}`, `${data.managerOfficeNumber}`);
     cards = cards.push(manager); 
