e5bc9e0218c0fc4964e2fbc2144d35e264d3fab2
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-10 20:59:10 -0500

    keep writing employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -24,5 +24,36 @@ describe('Employee', () => {
             const employee = new Employee("Jessie", 2323, email);
             expect(employee.email).toBe(email);
         });
+    });
+
+    describe('GET methods', () => {
+        it('should get a name via getName()', () => {
+            const name = "Jessie";
+            const employee = new Employee(name, 2323, 'jessie@test.com');
+            const employeeName = employee.getName();
+            expect(employeeName).toBe(name); 
+        });
+
+        it('should get id via getId()', () => {
+            const id = 2323;
+            const employee = new Employee('Jessie', id, "jessie@test.com");
+            const employeeId = employee.getId();
+            expect(employeeId).toBe(id);
+        });
+
+        it('should get email via getEmail()', () => {
+            const email = "jessie@test.com";
+            const employee = new Employee('Jessie', 2323, email);
+            const employeeEmail = employee.getEmail();
+            expect(employeeEmail).toBe(email);
+        });
+
+        it('should get role via getRole()', () => {
+            const role = "Employee";
+            const employee = new Employee('Jessie', 2323, 'jessie@test.com');
+            const employeeRole = employee.getRole();
+            expect(employeeRole).toBe(role);
+        });
     })
+
 })
